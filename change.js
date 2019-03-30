973a79e3afb264ea4615bbdf6b1fd7355da5e7d1
arifrahmanto <arifismiadi.rahmanto@mitrais.com>
2019-03-30 09:49:38 +0700

    Implement in nodejs

--- change.js
+++ change.js
@@ -1,4 +1,13 @@
 var cointypes = [200, 100, 50, 20, 10, 5, 2, 1];
+/**
+ * getChange accepts two parameters (totalPayable and cashPaid) and calculates
+ * the change in "coins" that needs to be returned.
+ * @param {number} totalPayable the integer amount (in pennies) to be paid
+ * @param {number} cashPaid the integer amount (in pennies) the person paid
+ * @returns {array} list of coins we need to dispense to the person as change
+ * @example
+ * getChange(215, 300); // returns [50, 20, 10, 5]
+ */
 function getChange(price, paid) {
   var difference = paid - price;
   var change = [];
@@ -12,4 +21,11 @@ function getChange(price, paid) {
   });
 
   return change;
+}
+
+/* The code block below ONLY Applies to Node.js - This Demonstrates
+   re-useability of JS code in both Back-end and Front-end! #isomorphic */
+/* istanbul ignore next */
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = getChange;  // allows CommonJS/Node.js require()
 }
